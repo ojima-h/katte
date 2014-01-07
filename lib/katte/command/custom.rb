b1c3d63f562a0f6f79f5c248c79c58f57a884e51
Hikaru Ojima <hikaru.ojima@mixi.co.jp>
2014-01-07 23:32:49 +0900

    bugfix

--- lib/katte/command/custom.rb
+++ lib/katte/command/custom.rb
@@ -1,8 +1,10 @@
 class Katte::Command
   class Custom
     class DSL
-      def initialize(receiver)
+      def initialize(receiver, out, err)
         @receiver = receiver
+        @out      = out
+        @err      = err
       end
 
       %w(done fail).each do |method|
@@ -13,11 +15,20 @@ class Katte::Command
       def tag(t)
         @receiver << [:next, t]
       end
+
+      def env      ; Katte.env         ; end
+      def env_hash ; Katte.env.to_hash ; end
+
+      def log(*args)
+        @err.puts args
+      end
     end
 
     def self.call(node)
-      Enumerator.new {|receiver|
-        DSL.new(receiver).instance_eval(File.read(node.path), node.path)
+      Katte::Command.open(node) {|out, err|
+        Enumerator.new {|receiver|
+          DSL.new(receiver, out, err).instance_eval(File.read(node.path), node.path)
+        }
       }
     end
   end
--- spec/katte/node/loader_spec.rb
+++ spec/katte/node/loader_spec.rb
@@ -4,7 +4,10 @@ class Katte::Node
   describe Loader do
     it "load all node under recipes root" do
       nodes = Loader.load(Katte.config.recipes_root)
-      expect(nodes.length).to eq 5
+
+      recipes_num = Dir.glob(File.join(Katte.config.recipes_root, '**', '*')).select{|f| FileTest.file? f}.length
+
+      expect(nodes.length).to eq recipes_num
       expect(nodes.map(&:name)).to include("test/sample")
       expect(nodes.map(&:name)).to include("test/sample/sub")
     end
