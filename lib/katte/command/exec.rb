22501291eaf063a6f5bf692e42e24e5cc823b7ae
Ojima Hikaru <ojiam.h@gmail.com>
2014-02-04 09:49:06 +0900

    Remove unneccessary command.

--- lib/katte/command/exec.rb
+++
@@ -1,7 +0,0 @@
-class Katte::Command::Exec
-  date  = ARGV.shift
-  files = ARGV.map {|path| FileTest.file? path }
-
-  app = Katte.new(datetime: date)
-  file.each {|file| app.exec file }
-end
--- lib/katte/command/run.rb
+++
@@ -1,6 +0,0 @@
-class Katte::Command::Run
-  date = ARGV.shift
-
-  app = Katte.new(datetime: date)
-  app.run
-end
